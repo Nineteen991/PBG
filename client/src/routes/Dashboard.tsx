a86446e7427ef7d0af51470c0deff58a04a4ba58
Nineteen991 <Nineteen99@gmail.com>
2023-05-03 18:27:39 -0700

    started on dashboard

--- client/src/routes/Dashboard.tsx
+++ client/src/routes/Dashboard.tsx
@@ -1,9 +1,23 @@
 import { useEffect, useState } from 'react'
 
 export default function Dashboard() {
+  const [addEvent, setAddEvent] = useState('')
+
+  
+
   return (
     <div className='dashboard'>
-      dashboard
+      <div className='add-event'>
+        <h1 className='section-title'>Add Event</h1>
+        <form className='add-event-form'>
+          <input
+            name='add-event'
+            className='dashboard-input'
+            // onChange={ handleChange }
+          />
+          <button className='add-event-btn btn'>Add Event</button>
+        </form>
+      </div>
     </div>
   )
 }
